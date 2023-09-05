1e04b4933953f74cca340d2120be82b4165b253e
Muqiu Han <muqiu-han@outlook.com>
2023-09-05 21:25:35 +0800

    Add: linenoise and result

---
+++ src/include/linenoise.hpp
@@ -0,0 +1,9 @@
+/* Copyright (c) 2022 Muqiu Han */
+
+#ifndef SINBUGER_LINENOISE_HPP
+#define SINBUGER_LINENOISE_HPP
+
+#include <cstddef>
+#include "linenoise.h"
+
+#endif /* SINBUGER_LINENOISE_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,6 @@
-#include "debugger.h"
-#include <iostream>
-#include <unistd.h>
-
 int
-main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Program name not specified";
-        return -1;
-    }
+main(int argc, char ** argv)
+{
 
-    auto prog = argv[1];
-    auto pid = fork();
-
-    if (pid == 0) {
-    } else if (pid >= 1) {
-        std::cout << "Started debugging process : " << pid << std::endl;
-        sinbuger::debugger dbg(prog, pid);
-        dbg.run();
-    }
-    return 0;
+  return 0;
 }

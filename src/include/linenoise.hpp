3be9049af441abbb5db5f8ff8ad00c618ef68b9e
Muqiu Han <muqiu-han@outlook.com>
2023-11-29 16:27:09 +0800

    [Refactor]: 🦀

--- src/include/linenoise.hpp
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2022 Muqiu Han */
-
-#ifndef SINBUGER_LINENOISE_HPP
-#define SINBUGER_LINENOISE_HPP
-
-#include "linenoise.h"
-#include <cstddef>
-
-#endif /* SINBUGER_LINENOISE_HPP */
